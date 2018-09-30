f2e4a836727293591aac72cc190f666dcf95d191
mihirsam <xenon.of.x@gmail.com>
2018-09-30 16:30:37 +0530

    complete checkBox

---
+++ JavaFX/JavaFX/src/sample/ChoiceBoxMain.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.application.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.Scene;
+
+public class ChoiceBoxMain extends Application
+{
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage)
+    {
+        window = primaryStage;
+        window.setTitle("Choice Box");
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("Apple", "Ball", "Cat");
+        choiceBox.getSelectionModel().select(0);
+
+        Label label = new Label();
+        Button order = new Button("Order");
+        order.setOnAction(e -> {
+            label.setText("Ordered item is " +choiceBox.getValue());
+        });
+
+        VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(choiceBox, order, label);
+        layout.setPadding(new Insets(10, 10, 10, 10));
+
+        Scene scene = new Scene(layout, 200, 300);
+
+        window.setScene(scene);
+        window.show();
+    }
+}
